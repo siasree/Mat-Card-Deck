e18bbd4db74e783ecf6100976628a6ff51e22073
siasree <csvidya455@gmail.com>
2019-09-08 18:22:28 +0530

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,108 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+
+buttonActive1:boolean;
+buttonActive2:boolean;
+
+refer(){
+  this.buttonActive1=false;
+  this.buttonActive2=true;
+}
+apply(){
+  this.buttonActive1=true;
+  this.buttonActive2=false;
+}
+
+  pictures = [
+    {
+      id: 1,
+      identity:'#56078',
+      h4: '.net Developer',
+      content:'MAS',
+      experience:'1-3 years',
+      country:"Minnestona,USA",
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/8V46UZCS0V.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 2,
+      identity:'#56079',
+      content:'MAS',
+      experience:'3-5 years',
+      country:"Bangalore,India",
+      h4: 'Project Lead',
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/LTLE4QGRVQ.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 3,
+      identity:'#56080',
+      content:'MAS',
+      experience:'5-8 years',
+      country:"Minnestona,USA",
+      h4: 'MicrosoftLead',
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/R926LU1YEA.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 4,
+      identity:'#56081',
+      content:'MAS',
+      experience:'10+ years',
+      country:"Yokihama,JAPAN",
+      h4: 'AzureLead',
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/U9PP3KXXY2.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 5,
+      // identity:'#56082',
+      h4: '.net Developer',
+      content:'MAS',
+      experience:'1-3 years',
+      country:"Minnestona,USA",
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/8V46UZCS0V.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 6,
+      // identity:'#56083',
+      content:'MAS',
+      experience:'3-5 years',
+      country:"Bangalore,India",
+      h4: 'Project Lead',
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/LTLE4QGRVQ.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 7,
+      // identity:'#56084',
+      content:'MAS',
+      experience:'5-8 years',
+      country:"Minnestona,USA",
+      h4: 'MicrosoftLead',
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/R926LU1YEA.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+    {
+      id: 8,
+      // identity:'#56085',
+      content:'MAS',
+      experience:'10+ years',
+      country:"Yokihama,JAPAN",
+      h4: 'AzureLead',
+      img: 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/U9PP3KXXY2.jpg',
+      button:'REFER',
+      button2:'APPLY'
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatCheckboxModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule, 
+    MatIconModule, 
+    MatToolbarModule, 
+    MatButtonModule, MatFormFieldModule, 
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
